0343cb04a1cb362e90a63960b4ca6fcde6888e12
Lucas-Cunhaa <lucasgalvao260@gmail.com>
2026-04-19 21:22:40 -0300

    feat: add the query paramters handler

--- src/routes.js
+++ src/routes.js
@@ -1,32 +1,39 @@
 import { Task } from "./tasks.js";
 import { Database } from './database.js'
+import { buildPath } from "./utils/buildPath.js";
 
 const database = new Database();
 
 export const routes = {
-    GET:  {
-            "/api/tasks": async(req, res) => {
-                const tasks = database.select('tasks')
+    GET:  [
+            {
+                path: buildPath("/api/tasks"),
+                handler: async(req, res) => {
+                    const tasks = database.select('tasks')
 
-                return res.writeHead(200).end(JSON.stringify(tasks))
-            } 
-        },
+                    return res.writeHead(200).end(JSON.stringify(tasks))
+                } 
+            }
+        ],
 
-    POST: 
-        {
-            "/api/tasks": async(req, res) => {
-                const { title, description } = req.body 
-                if(!title && !description) 
-                    return res.writeHead(400).end('Criterias are missing')
+    POST: [
+            {
+                path: buildPath("/api/tasks"), 
+                handler: async(req, res) => {
+                    const { title, description } = req.body 
+                    if(!title && !description) 
+                        return res.writeHead(400).end('Criterias are missing')
         
-                const newTask  = new Task(title, description) 
-                const insertion = await database.insert("tasks", newTask)
+                    const newTask  = new Task(title, description) 
+                    const insertion = await database.insert("tasks", newTask)
 
-                if(insertion) return res.writeHead(200).end('Task inserted Sucefully')
-        
-                res.writeHead(404).end('Error while inserting')
+                    if(insertion) return res.writeHead(200).end('Task inserted Sucefully')
+            
+                    res.writeHead(404).end('Error while inserting')
+
+                 }
             } 
-        },
+        ],
 
   PUT: [{ path: "/api/tasks", handler: "" }],
 
--- src/server.js
+++ src/server.js
@@ -1,21 +1,28 @@
 import http from 'node:http'
 import { routes } from './routes.js'
 import { jsonParse } from './middlewares/jsonParse.js'
+import { extractQueryParams } from './utils/extractQueryParams.js'
 
 const server = http.createServer(async (req, res) => {
     const { method, url } = req
     
     await jsonParse(req, res)
-    try {
-        const route = routes[method][url]
 
-        return route(req, res)
-    } catch(err) {
-        return res.writeHead(404).end()
+    const route = routes[method].find(route => {
+        return route.path.test(url)
+    })
+    
+    if(route) {
+        const routeParams = req.url.match(route.path)
+        const { query, ...params } = routeParams.groups
+
+        req.params = params
+        req.query = query ? extractQueryParams(query) : {}
+        
+        return route.handler(req, res)
     }
-    
 
-    
+    return res.writeHead(404).end()
 })
 
 server.listen(3000)
